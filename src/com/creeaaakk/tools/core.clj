2c05ed4b54fbd6bcca9d0aa8714a9190d4685dcd
Daniel Tashjian <thewopple@gmail.com>
2013-03-06 20:52:03 -0500

    Added template code.

--- src/com/creeaaakk/tools/core.clj
+++ src/com/creeaaakk/tools/core.clj
@@ -1,6 +1,16 @@
 (ns com.creeaaakk.tools.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-updater
+  ([start update]
+   (make-updater start update identity))
+
+  ([start update view]
+   (let [value (atom start)]
+     (fn []
+       (let [prev @value]
+         (swap! value update)
+         (view prev))))))
+
+(defn make-counter
+  [start]
+  (make-updater start inc))
---
+++ src/com/creeaaakk/tools/template.clj
@@ -0,0 +1,36 @@
+(ns com.creeaaakk.tools.template)
+
+(declare gen gen-list)
+
+(defn- seq-vec?
+  [sv]
+  (or (seq? sv) (vector? sv)))
+
+(defn- is-var?
+  [v]
+  (and (symbol? v) (= \? (first (name v)))))
+
+(defn gen
+  ([data template formats]
+   (gen data template formats identity))
+
+  ([data template formats join]
+   (join
+     (for [expr template]
+       (cond
+         (is-var? expr) ((formats expr) (data expr))
+         (fn? expr) (expr)
+         (seq-vec? expr) (let [[t k & expr] expr
+                               data (data k)]
+                           (case t
+                             :expr (gen data expr formats) 
+                             :list (gen-list expr case-map data)
+                             :fn (data)
+                             (format "** ERROR %s not recognized **" t)))
+         :else expr)))))
+
+(defn- gen-list
+  [data template formats join]
+  (join
+    (for [datum data]
+      (gen datum template formats))))
