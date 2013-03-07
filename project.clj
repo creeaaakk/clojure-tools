e886cc0a4fada611f164b81e8d67a03d37a7960a
Daniel Tashjian <thewopple@gmail.com>
2013-03-06 20:51:07 -0500

    Updated the project.clj file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-tools-temp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Tools for clojure"
+  :url ""
+  :license {:name "New BSD License"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
